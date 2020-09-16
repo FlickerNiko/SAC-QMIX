e66a9c79d675f632e9aa59bbeed5307cbd3a3798
wildniko <wildniko@outlook.com>
2020-09-16 11:22:23 +0800

    args modify

--- MQ/rnn_agent.py
+++ MQ/rnn_agent.py
@@ -6,20 +6,26 @@ import torch.nn.functional as F
 class RNNAgent(nn.Module):
     def __init__(self, args):
         super(RNNAgent, self).__init__()
-        self.args = args
-        self.fc1 = nn.Linear(args.input_dim + args.msg_dim + args.n_actions, args.rnn_hidden_dim)
-        self.rnn = nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
-        self.fc2 = nn.Linear(args.rnn_hidden_dim, args.n_actions)
-        self.fcm = nn.Linear(args.rnn_hidden_dim, args.msg_dim)
+        self.input_dim = args.input_dim
+        self.msg_dim = args.msg_dim
+        self.n_actions = args.n_actions
+        self.rnn_hidden_dim = args.rnn_hidden_dim
+        
+
+        self.fc1 = nn.Linear(self.input_dim + self.msg_dim + self.n_actions, self.rnn_hidden_dim)
+        self.rnn = nn.GRUCell(self.rnn_hidden_dim, self.rnn_hidden_dim)
+        self.fc2 = nn.Linear(self.rnn_hidden_dim, self.n_actions)
+        self.fcm = nn.Linear(self.rnn_hidden_dim, self.msg_dim)
+
     def init_hidden(self, n_batch):
         # make hidden states on same device as model
-        return self.fc1.weight.new_zeros(n_batch, self.args.rnn_hidden_dim)
+        return self.fc1.weight.new_zeros(n_batch, self.rnn_hidden_dim)
 
     def forward(self, inputs, message, last_action, hidden_state):
         x = torch.cat([inputs,message, last_action],1)
         x = F.relu(self.fc1(x))
-        h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
-        h = self.rnn(x, h_in)
+        #h_in = hidden_state.reshape(-1, self.rnn_hidden_dim)
+        h = self.rnn(x, hidden_state)
         q = self.fc2(h)
         m = F.tanh(self.fcm(h))
         return q, m, h
